dbe2147d8cefd86dff24fd7248f54888fd986c76
u2002022 <rhys.cowlam@warwick.ac.uk>
2022-01-21 13:13:19 +0000

    Created Header UI elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+  ]
+})
+export class MaterialModule { }
